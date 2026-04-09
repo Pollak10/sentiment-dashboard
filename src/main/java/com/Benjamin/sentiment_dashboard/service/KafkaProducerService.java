bc97da285d0e2714143753cdf53fe74f4b463bd9
Benjamin pollak <benjaminpollak@benjamins-air.mynetworksettings.com>
2026-04-09 19:48:59 -0400

    Replace KafkaTemplate with ApplicationEventPublisher

--- src/main/java/com/Benjamin/sentiment_dashboard/service/KafkaProducerService.java
+++ src/main/java/com/Benjamin/sentiment_dashboard/service/KafkaProducerService.java
@@ -1,20 +1,20 @@
 package com.Benjamin.sentiment_dashboard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
 
-    private static final String TOPIC = "sentiment-topic";
-
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private ApplicationEventPublisher eventPublisher;
 
     public void sendMessage(String symbol, String content) {
         String message = symbol + "||" + content;
-        kafkaTemplate.send(TOPIC, message);
-        System.out.println("Sent to kafka: " + message);
+        System.out.println("Publishing event: " + message);
+        eventPublisher.publishEvent(
+                new SentimentEvent(this, message)
+        );
     }
 }
